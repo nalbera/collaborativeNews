711f38d966b7625dfd002f43d7a4ba044c688fb2
Nelson Albera <nalbera@hotmail.com>
2025-06-27 14:58:46 -0300

    configure database connection and creation of entities and their relationships

--- api-service/src/app.module.ts
+++ api-service/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config as dotenvConfig } from 'dotenv';
 import { UserModule } from './modules/user/user.module';
 import { NewsModule } from './modules/news/news.module';
 import { VotesModule } from './modules/votes/votes.module';
 
+dotenvConfig({ path: '.env.development' });
+
 @Module({
-  imports: [UserModule, NewsModule, VotesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+    NewsModule,
+    VotesModule,
+  ],
   controllers: [],
   providers: [],
 })
